5b4070eb4d59f1a4f826acb9990e30a834329e47
thejas.n <thejasn619@gmail.com>
2018-07-09 23:01:47 +0530

    Update sum.java
    checks if any two elements of the array adds upto 10.

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum {
     public static void main(String[] args) {
 
         int[] a = new int[]{4, 1, 6, 8};
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i< 4; i++) {
             for (int j = i + 1; j < 4; j++) {
                 if (a[i] + a[j] == 10) {
                     int sum=a[i]+a[j];
